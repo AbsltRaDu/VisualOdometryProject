362a5eb378a45daf9aca6526dadfaf38ba1600d0
AbsltRaDu <jareniyponchik.da@gmail.com>
2026-04-23 16:02:04 +0300

    Начал писать класс TrajectoryTorch для удобного формата работы с траекторией и прошлыми позами

--- src/metrics/KITTI_metrics.py
+++ src/metrics/KITTI_metrics.py
@@ -7,3 +7,13 @@ def translation_rmse_drift(p_pred, p_fact, path_lenght):
 def rotation_rmse_drift(r_pred, r_fact, path_lenght):
     drift = torch.linalg.norm(r_pred - r_fact) 
     return torch.mean(drift / path_lenght) * 100
+
+def rmse_pos(p_pred: torch.Tensor, p_fact: torch.Tensor) -> torch.Tensor:
+    '''
+    RMSE по координатам
+    '''
+    
+    pred = p_pred[..., :3]
+    fact  = p_fact[..., :3]
+    
+    
