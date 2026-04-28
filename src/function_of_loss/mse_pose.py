a0a75d3521e431809775af13fc58653f325c5e35
AbsltRaDu <jareniyponchik.da@gmail.com>
2026-04-28 14:59:52 +0300

    Добавил оценку ошибки Траектории

--- src/function_of_loss/mse_pose.py
+++ src/function_of_loss/mse_pose.py
@@ -1,7 +1,14 @@
 import torch
 import torch.nn as nn 
 
+from src.geometry.PoseTorch import PoseTorch
+from src.geometry.TrajectoryTorch import TrajectoryTorch
+
 class PoseLoss(nn.Module):
+    '''
+    Ошибка по смещнию покадровому
+    '''
+    
     def __init__(self, k: float = 100, reduction: str = 'mean'):
         super().__init__()
         self.k = k # весовой коэф. для балансировки вкладов перемещения и вращения
@@ -20,15 +27,28 @@ class PoseLoss(nn.Module):
         
         return self.pos_loss + self.k * self.r_loss
     
-class PoseLossTrajectory(PoseLoss):
+class PoseLossTrajectory(nn.Module):
+    '''
+    Ошибка по конечной позе
+    '''
     
-    def __init__(self, k: float = 100, reduction: str = 'mean'):
-        super().__init__(self, k=k, reduction=reduction)
+    def __init__(self, a: float = 100, b: float = 1, reduction: str = 'sum'):
+        super().__init__()
+        self.mse = nn.MSELoss(reduction=reduction)
+        self.a = a
+        self.b = b
         
-    def forward(self, y_pred: torch.Tensor, y_fact: torch.Tensor):
-        '''
-        y_pred.size = (B, S, Dim)
-        '''
-        pass
+    def forward(self, y_pred: TrajectoryTorch, y_fact: PoseTorch):
+        
+        r_pred = y_pred.poses.R.as_quat()
+        t_pred = y_pred.poses.t
+        
+        r_fact = y_fact.R.as_quat()
+        t_fact = y_fact.t
+        
+        self.r_loss = self.mse(r_pred, r_fact)
+        self.t_loss = self.mse(t_pred, t_fact)
+        
+        return self.a * self.r_loss + self.b * self.t_loss
 
      
