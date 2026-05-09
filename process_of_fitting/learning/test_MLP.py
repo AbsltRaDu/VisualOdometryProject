2e61065d49bb5d4d6d6343ed4d4f8f89fc027575
AbsltRaDu <jareniyponchik.da@gmail.com>
2026-05-09 11:49:54 +0300

    Переобучил RAFT+CNN

--- process_of_fitting/learning/test_MLP.py
+++ process_of_fitting/learning/test_MLP.py
@@ -35,16 +35,16 @@ lst_of_dataset_train = lst_of_dataset[:-1]
 lst_of_dataset_test = lst_of_dataset[-1]
 
 
-dataset_train = mavDatasetCNN_RAFT('datasets/simulation', transform, normalize=normalize, device='cpu', lst_of_datasets=lst_of_dataset_train) # Сразу формируем все массивы на GPU
-dataset_test = mavDatasetCNN_RAFT('datasets/simulation', transform, normalize=normalize, device='cpu', lst_of_datasets=lst_of_dataset_test)
+dataset_train = mavDatasetCNN_RAFT('datasets/simulation', transform, normalize=normalize, device='cpu', lst_of_datasets=lst_of_dataset_test, max_size=30) # Сразу формируем все массивы на GPU
+dataset_test = mavDatasetCNN_RAFT('datasets/simulation', transform, normalize=normalize, device='cpu', lst_of_datasets=lst_of_dataset_test, max_size=30)
 
-WINDOW_SIZE = 20
+WINDOW_SIZE = 1
 
 dataset_train = SequenceDataset_RAFT(dataset_train, seq=5)
 dataset_test = SequenceDataset_RAFT(dataset_test, seq=5)
 
-train_sampler = ProgressiveWindowBatchSampler(dataset_train, batch_size=32, window_size=WINDOW_SIZE, shuffle=True)
-test_sampler = ProgressiveWindowBatchSampler(dataset_test, batch_size=32, window_size=WINDOW_SIZE, shuffle=True)
+train_sampler = ProgressiveWindowBatchSampler(dataset_train, batch_size=6, window_size=WINDOW_SIZE, shuffle=True)
+test_sampler = ProgressiveWindowBatchSampler(dataset_test, batch_size=6, window_size=WINDOW_SIZE, shuffle=True)
 
 train_data = data.DataLoader(dataset_train, batch_sampler=train_sampler, num_workers=6, pin_memory=True)
 test_data = data.DataLoader(dataset_test, batch_sampler=test_sampler, num_workers=6, pin_memory=True)
@@ -68,45 +68,69 @@ if os.path.isfile('process_of_fitting/fitting_models/CNN_RAFT.tar'):
 
 encoder = RAFTPoseCNNEncoder(raft=raft.flow_model, cnn=raft.CNN)
 encoder = encoder.to(device)
-model = DeepVO_RAFT(encoder=encoder, feat_dim=128, hidden_size=256, dropout=0.3)
+model = DeepVO_RAFT(encoder=encoder, feat_dim=128, hidden_size=256, dropout=0)
 model = model.to(device)
 
-if os.path.isfile('process_of_fitting/fitting_models/RCNN_RAFT.tar'):
-    state_dict_cnn = torch.load('process_of_fitting/fitting_models/RCNN_RAFT.tar', map_location=device)
-    model.load_state_dict(state_dict_cnn)
-    print('Были загружены веса модели с контрольной точки')
-
-# заморозили RAFT
-for p in model.encoder.flow_model.parameters():
-    p.requires_grad = False
-
-epochs = 20
 loss_func = PoseLoseSeq(k=1)
 loss_func_trajectory = PoseLossTrajectorySeq()
-optimizer = torch.optim.Adam([
-    {
-        "params": model.encoder.parameters(),
-        "lr": 1e-4
-    },
-    {
-        "params": model.head.rnn.parameters(),
-        "lr": 1e-4
-    },
-    {
-        "params": model.head.fc1.parameters(),
-        "lr": 1e-4
-    },
-    {
-        "params": model.head.fc2.parameters(),
-        "lr": 1e-4
-    }
-])
 
-count_of_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+img1, img2, y, pose = next(iter(train_data))
 
-print('Кол-во обучаемых параметров модели:', count_of_params, sep=' ')
+img1 = img1.to(device)
+img2 = img2.to(device)
+y = y.to(device).float()
 
-dct_of_results = training_RCNN_progressive_JointTrain_RAFT(train_data, train_data, model, loss_func_pose=loss_func, loss_func_trajectory=loss_func_trajectory, optimizer=optimizer, \
-    epochs=epochs, device=device, normalize=None, name_of_model=os.path.join('process_of_fitting/fitting_models', 'RCNN_RAFT.tar'), \
-        path_to_save_process_of_fitting=os.path.join('process_of_fitting/result_of_fitting', 'RCNN_RAFT.json'), squueze=False, window_size=WINDOW_SIZE, weight_trajectory=0)
+model.eval()
+with torch.no_grad():
+    S = img1.shape[1] # берем длину последовательности
+    features = []
+    
+    for s in range(S):
+        img1_s = img1[:, s] # (B, C, H, W)
+        img2_s = img2[:, s]
+        feat_s = model.encoder(img1_s, img2_s)
+        features.append(feat_s.flatten(1))
+features = torch.stack(features, dim=1) # (B, S, F)    
+
+print(features.shape, features.mean(), features.std())
+
+head = torch.nn.Sequential(
+    torch.nn.Linear(features.shape[-1], 256),
+    torch.nn.ReLU(),
+    torch.nn.Linear(256, 6)
+).to(device)
+
+opt = torch.optim.AdamW(model.head.parameters(), lr=1e-3)
+
+for name, p in model.named_parameters():
+    if "rnn" in name or "fc" in name:
+        print(name, p.requires_grad)
+
+for i, group in enumerate(opt.param_groups):
+    n_params = sum(p.numel() for p in group["params"])
+    n_trainable = sum(p.numel() for p in group["params"] if p.requires_grad)
+    print(i, "lr =", group["lr"], "params =", n_params, "trainable =", n_trainable)
+
+model.train()
+for i in range(1000):
+    
+    w_before = model.head.rnn.weight_ih_l0.detach().clone()
+    opt.zero_grad()
+
+    pred, _ = model(img1, img2, hidden=None)
+
+    loss = loss_func(pred.float(), y.float())
+
+    loss.backward()
+    opt.step()
+    
+    
+    w_after = model.head.rnn.weight_ih_l0.detach().clone()
+    
+    if i % 50 == 0:
+        print(i, loss.item())
+        print("RNN weight change:", (w_after - w_before).abs().max())
+        print("grad norm:", model.head.rnn.weight_ih_l0.grad.norm())
+
+
 
---
+++ tests_and_visualization/test_RCNN_RAFT.py
@@ -0,0 +1,110 @@
+import torch
+from torch.utils import data
+from torchvision import transforms as T
+import os
+
+from tqdm import tqdm
+
+import plotly.graph_objects as go
+
+from src.dataloaders.datasets_for_CNN_RAFT import mavDatasetCNN_RAFT, SequenceDataset_RAFT
+from src.dataloaders.Samplers import ProgressiveWindowBatchSampler
+from src.models.CNN_RAFT import RAFTPoseCNN, RAFTPoseCNNEncoder
+from src.models.DeepVO import DeepVO_RAFT
+from src.normalize.PoseNormolizerLie import PoseNormalizerLie
+from src.geometry.PoseTorch import PoseTorch as PT
+from src.geometry.TrajectoryTorch import TrajectoryTorch as TT
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.is_available():
+    print('Инференс на:', torch.cuda.get_device_name(torch.cuda.current_device()), sep=' ')
+else:
+    print('Инференс на', device, sep=' ')
+
+transform = T.Compose([
+    T.Resize((192, 320)),
+    T.Normalize(mean=[-1], std=[1])
+])
+
+# # normalize = PoseNormalizerLie()
+# # normalize.load('process_of_fitting/normalize_params/params_of_normalize.json')
+normalize = None
+
+lst_of_dataset = os.listdir('datasets/simulation')
+lst_of_dataset_train = lst_of_dataset[-2]
+lst_of_dataset_test = lst_of_dataset[-1]
+
+dataset = mavDatasetCNN_RAFT('datasets/simulation', transform, normalize=normalize, device='cpu', lst_of_datasets=lst_of_dataset_test, max_size=30) # Сразу формируем все массивы на GPU
+dataset = SequenceDataset_RAFT(dataset, seq=1)
+dtrain = data.DataLoader(dataset=dataset, batch_size=1)
+
+print('Длина датасета:', len(dataset), sep=' ')
+
+raft = RAFTPoseCNN()
+encoder = RAFTPoseCNNEncoder(raft=raft.flow_model, cnn=raft.CNN)
+model_cnn = DeepVO_RAFT(encoder=encoder)
+
+
+state_dict_cnn = torch.load('process_of_fitting/fitting_models/RCNN_RAFT_test.tar', map_location=device)
+model_cnn.load_state_dict(state_dict_cnn)
+model_cnn = model_cnn.to(device)
+
+trajectory = []
+trajectory_pred = []
+
+# normalize = normalize.to(device)
+
+hidden = None
+dtrain = iter(dtrain)
+model_cnn.eval()
+with torch.no_grad():
+    
+    img1, img2, y, pose = next(dtrain)
+    pose = pose.to(device)
+    y_pred, hidden = model_cnn(img1.to(device), img2.to(device), hidden)
+    y = y.to(device)
+    
+    if normalize:
+        y_pred = normalize.denormalize(y_pred)
+    
+    pose0 = PT.from_lie(pose)
+    trajectory = TT.from_lie_relative(y_pred, pose0)
+    trajectory_fact = TT.from_lie_relative(y, pose0)
+    
+    dataset_train = tqdm(dtrain, desc=f'Поехали', position=0)
+    for item in dataset_train:
+        img1, img2, y, _ = item
+        y = y.to(device)
+        
+        y_pred, hidden = model_cnn(img1.to(device), img2.to(device), hidden)
+        
+        if normalize:
+            y_pred = normalize.denormalize(y_pred)
+        
+        trajectory = trajectory.extend_lie_relative(y_pred)
+        trajectory_fact = trajectory_fact.extend_lie_relative(y)
+        
+trajectory_fact = trajectory_fact.positions().squeeze().cpu()
+trajectory = trajectory.positions().squeeze().cpu()
+
+print(trajectory)
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter3d(
+    x=[x[0] for x in trajectory_fact],
+    y=[x[1] for x in trajectory_fact],
+    z=[x[2] for x in trajectory_fact],
+    mode='lines',
+    name='Фактическая траектория'
+))
+
+fig.add_trace(go.Scatter3d(
+    x=[x[0] for x in trajectory[:-1]],
+    y=[x[1] for x in trajectory[:-1]],
+    z=[x[2] for x in trajectory[:-1]],
+    mode='lines',
+    name='Предсказанная траектория'
+))
+
+fig.show()
